063dcf06e8ecbc677ae6bccca50b31fc84a25d68
Manuel Morales <manuelmorales@gmail.com>
2015-10-03 16:51:56 +0100

    Added fake test

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-express = require('express');
-
+var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) { res.send('hola'); });
---
+++ test/spec/truth.spec.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+var request = require('supertest');
+
+describe('the truth', function () {
+    it('is true', function (done) {
+		done();
+    });
+});
