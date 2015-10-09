be962c73e4629cd733dd8675e2de81c4d6eb047c
Manuel Morales <manuelmorales@gmail.com>
2015-10-09 16:22:38 +0100

    Serializer extracted into its file

--- test/spec/tokensSerializer.spec.js
+++ test/spec/tokensSerializer.spec.js
@@ -1,21 +1,10 @@
 var chai = require('chai');
 var assert = chai.assert;
 
+var Serializer = require('../../app/TokensJsonSerializer')
+
 describe('tokensSerializer', function () {
 	beforeEach(function() {
-		var Serializer = function () { }
-		Serializer.prototype = {dump: function (token) {
-			var clean_token = {
-				uuid: token.uuid,
-				type: token.type,
-				content: token.content,
-				expiryDate: token.expiryDate,
-				createDate: token.createDate,
-				createUser: token.createUser
-			}
-			return JSON.stringify(clean_token);
-		}};
-
 		this.serializer = new Serializer();
 	});
 
