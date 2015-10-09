62c7b96d1f7dd383fa1ddf51add59b08b3e3abec
mariaclrd <mariaclrd@gmail.com>
2015-10-09 17:32:33 +0100

    Fixed broken test.

--- test/spec/actions/TokenActions.spec.js
+++ test/spec/actions/TokenActions.spec.js
@@ -3,15 +3,17 @@ var chai = require('chai');
 var assert = chai.assert;
 var tokenActions = require('../../../app/actions/TokenActions').tokenActions;
 
+var mongoose = require('mongoose');
+
 describe('TokenActions', function() {
     describe('create', function() {
         it('creates a token', function(done){
+            var connection = mongoose.connect("mongodb://localhost/fullstack-dev");
             tokenActions.create({
                 creator: 'pepe'
             }, function(){
-
-                assert.isEqual(arguments[0].createUser, "pepe");
-                done();
+                    assert.equal(arguments[0].createUser, "pepe");
+                    done();
             })
         });
     });
