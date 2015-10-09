bef4e372565e1b0a29bf49ef4496172a4b23bace
Jesus Prieto Colomina <chus1818@gmail.com>
2015-10-09 17:47:14 +0100

    WIP Refactors to the test

--- test/spec/authenticator.spec.js
+++ test/spec/authenticator.spec.js
@@ -5,8 +5,6 @@ var authenticator = require('../../app/authenticator');
 
 describe('authenticator', function () {
   beforeEach(function() {
-    var that = this;
-
     this.sessionid = 'mysessionid';
 
     this.req = {
@@ -30,24 +28,39 @@ describe('authenticator', function () {
     this.next = sinon.spy();
 
     this.url = 'http://qa.some_url.com/auth.json';
+
     this.error = undefined;
     this.responseStatusCode = 200;
-    this.response = { statusCode: this.responseStatusCode }
+
+    this.buildResponse = function(){
+      this.response = { statusCode: this.responseStatusCode };
+    };
 
     this.buildRequest = function(){
-      that.request = function(opts, callback){
-        callback(that.error, that.response);
+      var that = this;
+
+      this.request = function(opts, callback){
+        var validUrl = (opts.url == that.url);
+        var cookie = opts.headers['Cookie'];
+        var validCookie = cookie.indexOf('qa_session_id=mysessionid;') > -1;
+
+        if(validUrl && validCookie) {
+          callback(that.error, that.response);
+        } else {
+          throw 'Unexpected request params';
+        }
       };
     };
 
     this.buildAuthenticator = function(config){
-      config = config || { url: that.url, request: that.request };
-      that.authenticator = authenticator(config);
+      config = config || { url: this.url, request: this.request };
+      this.authenticator = authenticator(config);
     };
   });
 
   describe('when logged in in the authentication service', function() {
     beforeEach(function(){
+      this.buildResponse();
       this.buildRequest();
       this.buildAuthenticator();
     });
@@ -55,37 +68,63 @@ describe('authenticator', function () {
     it('calls next', function () {
       this.authenticator(this.req, this.res, this.next);
 
-      assert(this.next.called === true, 'Next was not called');
+      assert(this.next.called, 'Next was not called');
     });
   });
 
-  describe('when not logged in in the authentication service', function() {
-    beforeEach(function(){
-      var that = this;
-      this.error = true;
-      this.responseStatusCode = 401;
-      this.response = { statusCode: this.responseStatusCode }
-      this.request = function(opts, callback){
-        callback(that.error, that.response);
-      };
-      this.buildAuthenticator({request: this.request, url: this.url});
+  describe('when the authorization is not successful', function() {
+    describe('when there is an error during the request', function(){
+      beforeEach(function(){
+        this.error = true;
+
+        this.buildResponse();
+        this.buildRequest();
+        this.buildAuthenticator();
+      });
+
+      it('does not call next', function () {
+        this.authenticator(this.req, this.res, this.next);
+
+        assert(!this.next.called, 'Next was called');
+      });
+
+      it('responds with an error', function() {
+        this.authenticator(this.req, this.res, this.next);
+
+        assert(this.resStatus.calledWith(401), '401 was not returned');
+        assert(this.resJson.called, 'Error message was not returned');
+        assert(this.resJson.calledWith({error: {message: 'Auth error'}}),
+          'Error message was not returned in format'
+        );
+        assert(this.resEnd.called, 'Response was not ended');
+      });
     });
 
-    it('does not call next', function () {
-      this.authenticator(this.req, this.res, this.next);
+    describe('when the response is not successful', function(){
+      beforeEach(function(){
+        this.responseStatusCode = 401;
 
-      assert(this.next.called === false, 'Next was called');
-    });
+        this.buildResponse();
+        this.buildRequest();
+        this.buildAuthenticator();
+      });
 
-    it('responds with an error', function() {
-      this.authenticator(this.req, this.res, this.next);
+      it('does not call next', function () {
+        this.authenticator(this.req, this.res, this.next);
 
-      assert(this.resStatus.calledWith(401) === true, '401 was not returned');
-      assert(this.resJson.called === true, 'Error message was not returned');
-      assert(this.resJson.calledWith({error: {message: 'Auth error'}}) === true,
-        'Error message was not returned in format'
-      );
-      assert(this.resEnd.called === true, 'Response was not ended');
+        assert(!this.next.called, 'Next was called');
+      });
+
+      it('responds with an error', function() {
+        this.authenticator(this.req, this.res, this.next);
+
+        assert(this.resStatus.calledWith(401), '401 was not returned');
+        assert(this.resJson.called, 'Error message was not returned');
+        assert(this.resJson.calledWith({error: {message: 'Auth error'}}),
+          'Error message was not returned in format'
+        );
+        assert(this.resEnd.called, 'Response was not ended');
+      });
     });
   });
 });
